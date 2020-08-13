b04de6f50b9ea8fb1b37fa9bb9aa30f30cc1ce90
SmushBall <singhdeepakkumar1489@gmail.com>
2020-08-13 22:12:46 +0530

    changes for config

--- whatsapp_msg.py
+++ whatsapp_msg.py
@@ -8,9 +8,6 @@ import os
 
 from twilio.rest import Client 
  
-#account_sid = 'AC4c1b1fcd91d7e3c599dea9cbbc8eda14' 
-#auth_token = '959fef34b2698e4a82aa2c041a9df25a' 
-
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token) 
